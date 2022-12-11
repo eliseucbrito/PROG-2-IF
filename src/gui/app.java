1569323fcf2a132ee41b06e9206f707c82f4c757
eliseubrito7z <eliseubrito776@gmail.com>
2022-12-10 21:37:02 -0300

    GUI Colorful (in progress)

--- src/gui/app.java
+++ src/gui/app.java
@@ -7,18 +7,8 @@ import excecao.FullVectorException;
 import excecao.ReserveNotFoundedException;
 import fachada.Sae;
 
-import java.util.Date;
-import java.util.Date;
 import java.util.Scanner;
-
 public class app {
-
-    // Declaring ANSI_RESET so that we can reset the color
-    public static final String ANSI_RESET = "\u001B[0m";
-
-    // Declaring the color
-    // Custom declaration
-    public static final String ANSI_GREEN = "\u001B[32m";
 
     // max teachers, keys and reserves.
     private static final int MAX = 100;
@@ -33,29 +23,32 @@ public class app {
         Reserve res[]; // for list reserves
 
         do {
-            System.out.println(ANSI_GREEN+"0 - Exit"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"1 - Register (Teacher)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"2 - Register (Key)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"3 - Register (Reservation)"+ANSI_RESET);
+            System.out.print(Color.GREEN_BOLD);
+            System.out.println("0 - Exit");
+            System.out.println("1 - Register (Teacher)");
+            System.out.println("2 - Register (Key)");
+            System.out.println("3 - Register (Reservation)");
             System.out.println("==============================");
-            System.out.println(ANSI_GREEN+"4 - Remove (Teacher)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"5 - Remove (Key)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"6 - Remove (Reservation)"+ANSI_RESET);
+            System.out.println("4 - Remove (Teacher)");
+            System.out.println("5 - Remove (Key)");
+            System.out.println("6 - Remove (Reservation)");
             System.out.println("==============================");
-            System.out.println(ANSI_GREEN+"7 - Change (Teacher)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"8 - Change (Key)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"9 - Change (Reservation)"+ANSI_RESET);
+            System.out.println("7 - Change (Teacher)");
+            System.out.println("8 - Change (Key)");
+            System.out.println("9 - Change (Reservation)");
             System.out.println("==============================");
-            System.out.println(ANSI_GREEN+"10 - Consult (Teacher)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"11 - Consult (Key)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"12 - Consult (Reservation)"+ANSI_RESET);
+            System.out.println("10 - Consult (Teacher)");
+            System.out.println("11 - Consult (Key)");
+            System.out.println("12 - Consult (Reservation)");
             System.out.println("==============================");
-            System.out.println(ANSI_GREEN+"13 - List (Teacher)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"14 - List (Key)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"15 - List (Reservation)"+ANSI_RESET);
-            System.out.println(ANSI_GREEN+"Enter your option: "+ANSI_RESET);
+            System.out.println("13 - List (Teacher)");
+            System.out.println("14 - List (Key)");
+            System.out.println("15 - List (Reservation)");
+            System.out.print(Color.RESET);
+            System.out.print("Enter your option: ");
             op = input.nextInt();
             input.nextLine();
+
             switch (op) {
                 case 0:
                     System.out.println("Saindo...");
@@ -65,9 +58,9 @@ public class app {
                         System.out.println("Nome do professor: ");
                         name = input.nextLine();
                         tea = new Teacher(name);
-                        System.out.println("Operação realizada com sucesso");
+                        System.out.println(Color.GREEN_BACKGROUND+"Operação realizada com sucesso"+Color.RESET);
                     } catch (Exception e) {
-                        System.out.println("ERROR Vetor cheio :" + e.getMessage());
+                        System.out.println(Color.RED_BACKGROUND+"ERROR Vetor cheio :" + e.getMessage()+Color.RESET);
                     }
                 case 2:
                     // register key
@@ -88,12 +81,14 @@ public class app {
                         System.out.print("ID: ");
                         id = input.nextLine(); // hour in miliseconds
 //                        tea = new Teacher(name);
-                        System.out.println("NOME: "+name+"CHAVE: "+key+"ATIVIDADE: "+goal+" "+soliHour+devolHour+id);
                         re = new Reserve(name, key, goal, soliHour, devolHour, id);
-                        System.out.println("RESERVA"+re);
                         sae.registerReservation(re);
+
+                        System.out.println(Color.WHITE_BOLD);
+                        System.out.println(Color.GREEN_BACKGROUND+"Operação realizada com sucesso"+Color.RESET);
                     } catch (Exception e) {
-                        System.out.println("ERROR: " + e.getMessage());
+                        System.out.println(Color.WHITE_BOLD);
+                        System.out.println(Color.RED_BACKGROUND+"ERROR: " + e.getMessage()+Color.RESET);
                     }
                     break;
                 case 4:
@@ -108,9 +103,11 @@ public class app {
                         System.out.print("Digite o ID: ");
                         id = input.nextLine();
                         sae.removeReserve(id);
-                        System.out.println("Operação realizada com sucesso");
+                        System.out.println(Color.WHITE_BOLD);
+                        System.out.println(Color.GREEN_BACKGROUND+"Operação realizada com sucesso"+Color.RESET);
                     } catch (Exception e) {
-                        System.out.println("ERROR: "+e.getMessage());
+                        System.out.println(Color.WHITE_BOLD);
+                        System.out.println(Color.RED_BACKGROUND+"ERROR: "+e.getMessage()+Color.RESET);
                     }
                     break;
                 case 7:
@@ -144,7 +141,8 @@ public class app {
                         System.out.println("Hora de devolução: "+re.getDevolution_hour());
                         System.out.println("+++++++++++++++++++++++++++++++");
                     } catch (Exception e) {
-                        System.out.println("ERROR: " + e.getMessage());
+                        System.out.println(Color.WHITE_BOLD);
+                        System.out.println(Color.RED_BACKGROUND+"ERROR: " + e.getMessage()+Color.RESET);
                     }
                     break;
                 case 13:
@@ -153,20 +151,23 @@ public class app {
                     break;
                 case 15:
                     try {
-                        System.out.println("===== LISTAR RESERVAS =====");
                         res = sae.listReserves();
+
+                        System.out.printf("----------------------------------------------------------------%n");
+                        System.out.printf("                         LISTAR RESERVAS                        %n");
+                        System.out.printf("----------------------------------------------------------------%n");
+                        System.out.printf("| %-4s | %-10s | %-10s | %4s | %4s | %4s |%n",
+                                "ID", "PROFESSOR", "ATIVIDADE", "CHAVE", "H. SOLIC", "H. DEVOL");
+                        System.out.printf("----------------------------------------------------------------%n");
                         for (int i = 0; i < res.length; i++) {
-                            System.out.println("+++++++++++++++++++++++++++++++");
-                            System.out.println("ID: "+res[i].getId());
-                            System.out.println("Professor: "+res[i].getTeacher());
-                            System.out.println("Atividade: "+res[i].getActivity());
-                            System.out.println("Chave: "+res[i].getKey());
-                            System.out.println("Hora de solicitação: "+res[i].getSolicitation_hour());
-                            System.out.println("Hora de devolução: "+res[i].getDevolution_hour());
-                            System.out.println("+++++++++++++++++++++++++++++++");
+                            System.out.printf("| %-4s | %-10s | %-10s | %5s | %6s | %6s |%n",
+                                    res[i].getId(),  res[i].getTeacher(), res[i].getActivity(), res[i].getKey(),
+                                    res[i].getSolicitation_hour(), res[i].getDevolution_hour());
                         }
+                        System.out.printf("----------------------------------------------------------------%n");
                     } catch (Exception e) {
-                        System.out.println("ERROR: "+e.getMessage());
+                        System.out.println(Color.WHITE_BOLD);
+                        System.out.println(Color.RED_BACKGROUND+"ERROR: "+e.getMessage()+Color.RESET);
                     }
                     break;
                 default:
@@ -176,3 +177,24 @@ public class app {
     }
 
 }
+
+// CASE 15
+//try {
+//        System.out.printf("--------------------------------%n");
+//        System.out.printf("    LISTAR RESERVAS      %n");
+//        System.out.printf("--------------------------------%n");
+//        res = sae.listReserves();
+//        for (int i = 0; i < res.length; i++) {
+//        System.out.println("+++++++++++++++++++++++++++++++");
+//        System.out.println("ID: "+res[i].getId());
+//        System.out.println("Professor: "+res[i].getTeacher());
+//        System.out.println("Atividade: "+res[i].getActivity());
+//        System.out.println("Chave: "+res[i].getKey());
+//        System.out.println("Hora de solicitação: "+res[i].getSolicitation_hour());
+//        System.out.println("Hora de devolução: "+res[i].getDevolution_hour());
+//        System.out.println("+++++++++++++++++++++++++++++++");
+//        }
+//        } catch (Exception e) {
+//        System.out.println("ERROR: "+e.getMessage());
+//        }
+//        break;
